08bc94cdceab86a8cc34562efe895aac51885a03
pythias <pythias@gmail.com>
2017-01-04 16:30:59 +0800

    194. Transpose File

---
+++ transpose_file.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cat file.txt | awk '{
+    for (i = 1; i <= NF; i++) {
+        values[FNR, i] = $i
+    }
+} END {
+    for (i = 1; i <= NF; i++) {
+        line = ""
+        sep = ""
+        for (j = 1; j <= FNR; j++) {
+            line = sprintf("%s%s%s", line, sep, values[j, i])
+            sep = " "
+        }
+        print line
+    }
+}
+'
