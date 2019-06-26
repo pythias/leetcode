81482f03e2ba63e109e0fd19cc002c78e3139a16
pythias <pythias@gmail.com>
2019-06-26 21:04:09 +0800

    262. Trips and Users

--- database/trips_users.sql
+++ database/trips_users.sql
@@ -1,22 +1,26 @@
 # Write your MySQL query statement below
-CREATE TABLE `Trips` (
-  `Id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '',
-  `Client_Id` bigint(20) unsigned DEFAULT '1' COMMENT '',
-  `device_serial` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '',
-  `Driver_Id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '',
-  `City_Id` int(10) NOT NULL DEFAULT '0' COMMENT '',
-  `Status` enum ('completed', 'cancelled_by_driver', 'cancelled_by_client') DEFAULT 'completed' COMMENT '',
-  `Request_at` varchar(256) DEFAULT 'iPhone' COMMENT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `device_serial` (`device_serial`),
-  KEY `device_id` (`device_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+Create table If Not Exists Trips (Id int, Client_Id int, Driver_Id int, City_Id int, Status ENUM('completed', 'cancelled_by_driver', 'cancelled_by_client'), Request_at varchar(50));
+Create table If Not Exists Users (Users_Id int, Banned varchar(50), Role ENUM('client', 'driver', 'partner'));
+Truncate table Trips;
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('1', '1', '10', '1', 'completed', '2013-10-01');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('2', '2', '11', '1', 'cancelled_by_driver', '2013-10-01');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('3', '3', '12', '6', 'completed', '2013-10-01');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('4', '4', '13', '6', 'cancelled_by_client', '2013-10-01');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('5', '1', '10', '1', 'completed', '2013-10-02');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('6', '2', '11', '6', 'completed', '2013-10-02');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('7', '3', '12', '6', 'completed', '2013-10-02');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('8', '2', '12', '12', 'completed', '2013-10-03');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('9', '3', '10', '12', 'completed', '2013-10-03');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ('10', '4', '13', '12', 'cancelled_by_driver', '2013-10-03');
+insert into Trips (Id, Client_Id, Driver_Id, City_Id, Status, Request_at) values ("1", "1", "10", "1", "cancelled_by_client", "2013-10-04");
+Truncate table Users;
+insert into Users (Users_Id, Banned, Role) values ('1', 'No', 'client');
+insert into Users (Users_Id, Banned, Role) values ('2', 'Yes', 'client');
+insert into Users (Users_Id, Banned, Role) values ('3', 'No', 'client');
+insert into Users (Users_Id, Banned, Role) values ('4', 'No', 'client');
+insert into Users (Users_Id, Banned, Role) values ('10', 'No', 'driver');
+insert into Users (Users_Id, Banned, Role) values ('11', 'No', 'driver');
+insert into Users (Users_Id, Banned, Role) values ('12', 'No', 'driver');
+insert into Users (Users_Id, Banned, Role) values ('13', 'No', 'driver');
 
-CREATE TABLE `Users` (
-  `Users_Id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '',
-  `Banned` boolean DEFAULT 'No' COMMENT '',
-  `Role` enum ('client', 'driver', 'partner') DEFAULT 'client' COMMENT '',
-  PRIMARY KEY (`Users_Id`),
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
-
-SELECT `U`.`Request_at` AS `Day`, `U`.`Status` AS `Cancellation Rate` FROM `Trips` AS `T` INNER JOIN `Users` AS `U` ON `T`.`Client_Id` = `U`.`User_Id` WHERE `U`.`Role` == 'Client'
+SELECT `T`.`Request_at` AS `Day`, ROUND(SUM(CASE WHEN `T`.`Status` = 'completed' THEN 0 ELSE 1 END) / COUNT(*), 2) AS `Cancellation Rate` FROM `Trips` AS `T` INNER JOIN `Users` AS `U` ON `T`.`Client_Id` = `U`.`Users_Id` WHERE `U`.`Banned` = 'No' AND `T`.`Request_at` BETWEEN '2013-10-01' AND '2013-10-03' GROUP BY `T`.`Request_at`;
