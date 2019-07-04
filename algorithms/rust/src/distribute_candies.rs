7e7cc7c604fb3cabfdfd25c923ad83071ae39595
pythias <pythias@gmail.com>
2019-07-04 15:37:05 +0800

    1103. Distribute Candies to People

---
+++ algorithms/rust/src/distribute_candies.rs
@@ -0,0 +1,59 @@
+impl Solution {
+    pub fn distribute_candies(candies: i32, num_people: i32) -> Vec<i32> {
+        if num_people < 1 {
+            return vec![]
+        }
+
+        let num: usize = num_people as usize;
+        let mut value = vec![0; num];
+        if candies == 0 {
+            return value;
+        }
+
+        let mut remain: i32 = candies;
+        let mut count = 1;
+        let mut index: usize = 0;
+        loop {
+            if index >= num {
+                index = 0;
+            }
+
+            if count >= remain {
+                value[index] += remain;
+                return value;
+            }
+
+            value[index] += count;
+            remain -= count;
+            index += 1;
+            count += 1;
+        }
+    }
+}
+
+pub struct Solution {}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test0() {
+        assert_eq!(Solution::distribute_candies(7, 3), vec![2, 2, 3]);
+    }
+
+    #[test]
+    fn test1() {
+        assert_eq!(Solution::distribute_candies(10, 3), vec![5, 2, 3]);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(Solution::distribute_candies(11, 3), vec![5, 3, 3]);
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(Solution::distribute_candies(3, 4), vec![1, 2, 0, 0]);
+    }
+}
