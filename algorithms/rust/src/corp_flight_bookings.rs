9ed37e05ad181f581e9f63d1f0938968f2a3ae5d
pythias <pythias@gmail.com>
2019-07-11 14:40:36 +0800

    1109. Corporate Flight Bookings
    这题目的最优解真牛逼

---
+++ algorithms/rust/src/corp_flight_bookings.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
+        let mut value = vec![0; n as usize];
+        for booking in bookings {
+            let (f, t, v) = (booking[0], booking[1], booking[2]);
+            for i in (f-1)..std::cmp::min(t, n) {
+                value[i as usize] += v;
+            }
+        }
+
+        return value;
+    }
+}
+
+pub struct Solution {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(Solution::corp_flight_bookings(vec![vec![1, 2, 10], vec![2, 3, 20], vec![2, 5, 25]], 5), vec![10, 55, 45, 25, 25]);
+    }
+}
