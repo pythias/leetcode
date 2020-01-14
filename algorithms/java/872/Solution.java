6bc9d6d26bfda5301f082c48851d785a3697577b
pythias <pythias@gmail.com>
2020-01-14 19:31:52 +0800

    872. Leaf-Similar Trees

---
+++ algorithms/java/872/Solution.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+class TreeNode { 
+    int val; 
+    TreeNode left; 
+    TreeNode right; 
+    TreeNode(int x) { val = x; } 
+}
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> leafs1 = new ArrayList<Integer>();
+        this.getLeafs(root1, leafs1);
+
+        ArrayList<Integer> leafs2 = new ArrayList<Integer>();
+        this.getLeafs(root2, leafs2);
+
+        if (leafs1.size() != leafs2.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < leafs1.size(); i++) {
+            if (leafs1.get(i) != leafs2.get(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void getLeafs(TreeNode root, ArrayList<Integer> leafs) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left != null) {
+            this.getLeafs(root.left, leafs);
+        }
+        
+        if (root.right != null) {
+            this.getLeafs(root.right, leafs);
+        } 
+        
+        if (root.left == null && root.right == null) {
+            leafs.add(root.val);
+        }
+    }
+}
