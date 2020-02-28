fce00310b6d913feeadbfc2bd552cd36b6062d70
pythias <pythias@gmail.com>
2020-02-28 23:36:19 +0800

    146. LRU Cache 利用链表解决

--- algorithms/java/src/com/duo/leetcode/_0100/_146/LRUCache.java
+++ algorithms/java/src/com/duo/leetcode/_0100/_146/LRUCache.java
@@ -1,58 +1,85 @@
 package com.duo.leetcode._0100._146;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LRUCache {
+    static class ValueNode {
+        private ValueNode prev;
+        private ValueNode next;
+        private int key;
+        private int value;
+
+        public ValueNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public ValueNode() {
+
+        }
+    }
+
     private int capacity;
-    private int size;
-    private HashMap<Integer, Integer> cache;
-    private ArrayList<Integer> lurs;
+    private HashMap<Integer, ValueNode> cache;
+    private ValueNode head, tail;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.cache = new HashMap<Integer, Integer>();
-        this.size = 0;
-        this.lurs = new ArrayList<Integer>();
+        this.cache = new HashMap<Integer, ValueNode>();
+        this.head = new ValueNode();
+        this.tail = new ValueNode();
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
 
     public int get(int key) {
         if (this.cache.containsKey(key)) {
-            this.renew(key);
-            return this.cache.get(key);
+            ValueNode valueNode = this.cache.get(key);
+            this.moveToHead(valueNode);
+            return valueNode.value;
         }
 
         return -1;
     }
 
     public void put(int key, int value) {
-        if (this.size == this.capacity) {
-            if (!this.cache.containsKey(key)) {
-                int oldKey = this.lurs.remove(0);
-                this.cache.remove(oldKey);
-                this.lurs.add(key);
-            } else {
-                this.renew(key);
-            }
-        } else {
-            if (!this.cache.containsKey(key)) {
-                this.size++;
-                this.lurs.add(key);
-            } else {
-                this.renew(key);
-            }
+        if (this.cache.containsKey(key)) {
+            ValueNode valueNode = this.cache.get(key);
+            valueNode.value = value;
+            this.moveToHead(valueNode);
+            return;
         }
 
-        this.cache.put(key, value);
+        ValueNode valueNode = new ValueNode(key, value);
+        this.cache.put(key, valueNode);
+        this.newNode(valueNode);
+
+        if (this.cache.size() > this.capacity) {
+            this.removeTail();
+        }
     }
 
-    private void renew(int key) {
-        for (int i = 0; i < this.lurs.size(); i++) {
-            if (this.lurs.get(i) == key) {
-                this.lurs.remove(i);
-                break;
-            }
-        }
-        this.lurs.add(key);
+    private void moveToHead(ValueNode node) {
+        this.removeNode(node);
+        this.newNode(node);
+    }
+
+    private void newNode(ValueNode node) {
+        node.next = this.head.next;
+        node.prev = this.head;
+
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
+
+    private void removeNode(ValueNode node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
+
+    private void removeTail() {
+        ValueNode node = this.tail.prev;
+        this.removeNode(node);
+        this.cache.remove(node.key);
     }
 }
--- algorithms/java/test/com/duo/leetcode/TestLRU.java
+++ algorithms/java/test/com/duo/leetcode/TestLRU.java
@@ -23,12 +23,20 @@ public class TestLRU {
 
     @Test
     public void test1() {
-        LRUCache lruCache = new LRUCache(2);
-        lruCache.put(2, 1);
-        lruCache.put(2, 2);
-        Assert.assertEquals(2, lruCache.get(2));
+        LRUCache lruCache = new LRUCache(3);
         lruCache.put(1, 1);
-        lruCache.put(4, 1);
-        Assert.assertEquals(-1, lruCache.get(2));
+        lruCache.put(2, 2);
+        lruCache.put(3, 3);
+        lruCache.put(4, 4);
+        Assert.assertEquals(4, lruCache.get(4));
+        Assert.assertEquals(3, lruCache.get(3));
+        Assert.assertEquals(2, lruCache.get(2));
+        Assert.assertEquals(-1, lruCache.get(1));
+        lruCache.put(5, 5);
+        Assert.assertEquals(-1, lruCache.get(1));
+        Assert.assertEquals(2, lruCache.get(2));
+        Assert.assertEquals(3, lruCache.get(3));
+        Assert.assertEquals(-1, lruCache.get(4));
+        Assert.assertEquals(5, lruCache.get(5));
     }
 }
