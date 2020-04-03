e5f488d2ce22183763e3b56bb496106665a30366
pythias <pythias@gmail.com>
2020-04-03 18:58:10 +0800

    30-Day LeetCoding Challenge

--- algorithms/java/src/main/java/com/duo/leetcode/Main.java
+++ algorithms/java/src/main/java/com/duo/leetcode/Main.java
@@ -4,32 +4,35 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.ClassPath;
 
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public void run(String input) throws IOException {
         final ClassLoader loader = this.getClass().getClassLoader();
         final ImmutableSet<ClassPath.ClassInfo> classInfos = ClassPath.from(loader).getTopLevelClasses();
+        List<ClassPath.ClassInfo> sortedClasses = new ArrayList<ClassPath.ClassInfo>();
+        sortedClasses.addAll(classInfos.asList());
+        sortedClasses.sort(Comparator.comparing(ClassPath.ClassInfo::getName));
+
         String[] ids = input.split(",");
         for (String id : ids) {
-            String packageName = this.getAlgorithmClass(id);
+            String packageName = (id.equals("all") ? "com.duo.leetcode." : this.getAlgorithmClass(id));
             try {
-                for (final ClassPath.ClassInfo info : classInfos) {
+                for (final ClassPath.ClassInfo info : sortedClasses) {
                     if (!info.getName().startsWith(packageName)) {
                         continue;
                     }
 
                     final Class<?> clazz = info.load();
-                    if (!Algorithm.class.isAssignableFrom(clazz)) {
+                    if (!Algorithm.class.isAssignableFrom(clazz) || clazz.isInterface()) {
                         continue;
                     }
 
-                    System.out.printf("Run algorithm %s\n", id);
+                    System.out.printf("Run algorithm %s\n", clazz.getName().substring(17));
                     Algorithm algorithm = (Algorithm) clazz.newInstance();
                     algorithm.run();
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
+            } catch (IllegalAccessException | InstantiationException e) {
                 e.printStackTrace();
             }
         }
@@ -42,6 +45,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        main.run("394");
+        main.run("all");
     }
 }
