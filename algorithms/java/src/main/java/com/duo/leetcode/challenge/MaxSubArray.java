d4b86ca36f462ceb737b6098bdb94e5870ff8968
pythias <pythias@gmail.com>
2020-04-07 12:21:29 +0800

    30-day-leetcoding-challenge 第三题目

--- algorithms/java/src/main/java/com/duo/leetcode/challenge/MaxSubArray.java
+++ algorithms/java/src/main/java/com/duo/leetcode/challenge/MaxSubArray.java
@@ -1,4 +1,31 @@
 package com.duo.leetcode.challenge;
 
-public class MaxSubArray {
+import com.duo.leetcode.Algorithm;
+
+public class MaxSubArray implements Algorithm {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int i = 0;
+        while (i < nums.length) {
+            if (sum > 0 && sum + nums[i] > 0) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+
+            max = Math.max(max, sum);
+            i++;
+        }
+
+        return max;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.maxSubArray(new int[] { -2 }));
+        System.out.println(this.maxSubArray(new int[] { -2, -1 }));
+        System.out.println(this.maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        System.out.println(this.maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4, 2 }));
+    }
 }
---
+++ algorithms/java/src/main/java/com/duo/leetcode/challenge/MoveZeroes.java
@@ -0,0 +1,4 @@
+package com.duo.leetcode.challenge;
+
+public class MoveZeroes {
+}
