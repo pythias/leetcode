3e8d4c2075489e7224845f74f34479f32bacce93
pythias <pythias@gmail.com>
2020-04-14 20:30:34 +0800

    Contiguous Array

--- algorithms/java/src/main/java/com/duo/leetcode/Main.java
+++ algorithms/java/src/main/java/com/duo/leetcode/Main.java
@@ -43,6 +43,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        main.run("StringShift");
+        main.run("FindMaxLength");
     }
 }
--- algorithms/java/src/main/java/com/duo/leetcode/challenge/FindMaxLength.java
+++ algorithms/java/src/main/java/com/duo/leetcode/challenge/FindMaxLength.java
@@ -1,18 +1,37 @@
 package com.duo.leetcode.challenge;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.duo.leetcode.Algorithm;
 
 /**
  * Given a binary array, find the maximum length of a contiguous subarray with
  * equal number of 0 and 1.
- * 
- * 没读懂题目
  */
 public class FindMaxLength implements Algorithm {
     public int findMaxLength(int[] nums) {
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> pos = new HashMap<>();
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            sum += num == 0 ? -1 : 1;
+            if (sum == 0) {
+                max = i + 1;
+            } else if (pos.containsKey(sum)) {
+                max = Math.max(max, i - pos.get(sum));
+            } else {
+                pos.put(sum, i);
+            }
+        }
+
+        return max;
     }
 
     private void _run(int[] stones) {
@@ -23,6 +42,10 @@ public class FindMaxLength implements Algorithm {
 
     @Override
     public void run() {
-        this._run(new int[] { 3, 7, 8 });
+        this._run(new int[] { 0, 1, 0, 1, 0 });
+        this._run(new int[] { 0, 1, 0 });
+        this._run(new int[] { 0, 1 });
+        this._run(new int[] { 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 });
+        this._run(new int[] { 0, 1, 0, 0, 0, 1 });
     }
 }
