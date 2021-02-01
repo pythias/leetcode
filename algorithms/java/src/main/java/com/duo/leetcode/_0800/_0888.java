e17e5e8261741978d20845ace338b7c18c793759
pythias <pythias@gmail.com>
2021-02-01 23:16:41 +0800

    其他解

--- algorithms/java/src/main/java/com/duo/leetcode/_0800/_0888.java
+++ algorithms/java/src/main/java/com/duo/leetcode/_0800/_0888.java
@@ -1,8 +1,6 @@
 package com.duo.leetcode._0800;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * 888. Fair Candy Swap
@@ -10,6 +8,10 @@ import java.util.ArrayList;
  */
 public class _0888 {
     public int[] fairCandySwap(int[] A, int[] B) {
+        return this._solution2(A, B);
+    }
+
+    private int[] _solution0(int[] A, int[] B) {
         int leftSum = this._sum(A);
         int rightSum = this._sum(B);
         int half = (leftSum + rightSum) / 2;
@@ -27,7 +29,54 @@ public class _0888 {
         return new int[] {0, 0};
     }
 
+    private int[] _solution1(int[] A, int[] B) {
+        int leftSum = this._sum(A);
+        int rightSum = this._sum(B);
+        int half = (leftSum + rightSum) / 2;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            int left = half - leftSum + A[i];
+            for (int j = B.length - 1; j > -1; j--) {
+                int right = B[j];
+                if (right == left) {
+                    return new int[] {A[i], right}; 
+                }
+
+                if (right < left) {
+                    break;
+                }
+            }
+        }
+
+
+        return new int[] {0, 0};
+    }
+
+    private int[] _solution2(int[] A, int[] B) {
+        int leftSum = this._sum(A);
+        int rightSum = this._sum(B);
+        int delta = (rightSum - leftSum) / 2;
+        Set<Integer> diffs = new HashSet<Integer>();
+        for (int i : A) {
+            diffs.add(i);
+        }
+
+        for (int i : B) {
+            if (diffs.contains(i - delta)) {
+                return new int[] {i - delta, i};
+            }
+        }
+
+        return new int[] {0, 0};
+    }
+
     private int _sum(int[] array) {
-        return Arrays.stream(array).sum();
+        int s = 0;
+        for (int i : array) {
+            s += i;
+        }
+        return s;
     }
 }
