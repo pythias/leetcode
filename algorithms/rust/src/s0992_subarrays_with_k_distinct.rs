bed340a31b1f11ae3a41c884e326d47aa4909c1e
pythias <pythias@gmail.com>
2021-02-10 00:23:49 +0800

    contiguous

--- algorithms/rust/src/s0992_subarrays_with_k_distinct.rs
+++ algorithms/rust/src/s0992_subarrays_with_k_distinct.rs
@@ -1,7 +1,14 @@
 //992. K 个不同整数的子数组
 //https://leetcode-cn.com/problems/subarrays-with-k-different-integers/
+//少读了连续的字眼："contiguous, not necessarily distinct"
 impl Solution {
+    //窗口解法
     pub fn subarrays_with_k_distinct(a: Vec<i32>, k: i32) -> i32 {
+        
+    }
+
+    //最笨解法
+    pub fn s1(a: Vec<i32>, k: i32) -> i32 {
         let len = a.len();
         let uk = k as usize;
         let mut count = 0;
@@ -13,7 +20,10 @@ impl Solution {
             }
 
             for j in (i+1)..len {
-                if !sub.contains_key(&a[j]) && sub.len() < uk {
+                if !sub.contains_key(&a[j]) {
+                    if sub.len() == uk {
+                        break;
+                    }
                     sub.insert(a[j], 0);
                 }
                 
@@ -35,8 +45,6 @@ mod tests {
 
     #[test]
     fn test_0992() {
-        //明明是9，为何是8？
-        //[2] [2,2] [1] [1,1] [1,1,1] [1] [1,1] [1] [2]
         assert_eq!(Solution::subarrays_with_k_distinct(vec![2,1,1,1,2], 1), 8);
         assert_eq!(Solution::subarrays_with_k_distinct(vec![1,1,1], 1), 6);
         assert_eq!(Solution::subarrays_with_k_distinct(vec![1,2], 1), 2);
